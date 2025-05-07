e7a314a0b0c96138a57486123cb197bb8dbd98ae
Samuel Vasconcelos <sampvasc@gmail.com>
2025-05-07 07:44:07 -0300

    Adicionando o script do banco

---
+++ script.sql
@@ -0,0 +1,18 @@
+-- criando o banco de dados
+create database bdExemplo;
+
+-- Usando o banco de dados
+use bdExemplo;
+
+-- Criando as tabelas do banco
+create table Usuarios(
+Id int primary key auto_increment,
+Nome varchar(50) not null,
+Email varchar(50) not null,
+Senha varchar(50) not null
+);
+
+-- Consultando os dados do banco
+select * from Usuarios;
+
+	
